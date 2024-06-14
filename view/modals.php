1f863f50885ec71982a489d827c7092e7d09c8d2
Tris <trisnanugraha87@gmail.com>
2024-06-14 19:28:01 +0700

    Updated Bahan Baku

--- view/modals.php
+++ view/modals.php
@@ -133,3 +133,85 @@
   </div>
 </div>
 <!-- End Modal Tambah Data Pelanggan -->
+
+<!-- Modal Tambah Data Bahan Baku -->
+<div class="modal fade" id="tambah-data-bahan-baku" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tambahModalLabel">Tambah Data Bahan Baku</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post">
+          <div class="d-flex justify-content-center">
+            <img class="img-fluid" width="120px" src="../view/assets/img/material.png" rel="icon">
+          </div>
+
+          <h6 class="text-center">Silahkan isi data dengan lengkap di bawah ini. </h6>
+          <hr>
+
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="tambah_kode_bom">Kode BOM</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="kode_bom" id="tambah_kode_bom" required>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="tambah_id_produk">ID Produk</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="id_produk" id="tambah_id_produk" required>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="tambah_nama_bahan_baku">Nama Bahan Baku</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="nama_bahan_baku" id="tambah_nama_bahan_baku" required>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="tambah_jenis_bahan_baku">Jenis Bahan Baku</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="jenis_bahan_baku" id="tambah_jenis_bahan_baku" required>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="tambah_stok_awal">Stok Awal</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="stok_awal" id="tambah_stok_awal" required>
+            </div>
+          </div>
+
+          <div class="row mt-2">
+            <div class="col-3">
+              <label for="tambah_stok_akhir">Stok Akhir</label>
+            </div>
+            <div class="col-9">
+              <input type="text" class="form-control rounded-pill" name="stok_akhir" id="tambah_stok_akhir" required>
+            </div>
+          </div>
+
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <button type="button" class="btn btn-secondary rounded-pill" data-bs-dismiss="modal">Batalkan</button>
+        <button class="btn btn-primary rounded-pill" type="submit" name="tambah-bahan-baku">Simpan</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Modal Tambah Data Bahan Baku -->
